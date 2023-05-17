57e692236d6dcd7e58b45ff0851a101d3b2ffada
AMRⓗⓐⓧⓩ404 <amrin10122000@gmail.com>
2023-05-17 15:58:21 +0700

    Initial commit

---
+++ wp-content/themes/wp-masjid/footer.php
@@ -0,0 +1,16 @@
+            <div class="footer">
+                <div class="wm__container">
+	            	<div class="copyright">
+<!-- 						<?php text_footer(); ?> -->
+						Copyright © 2023 Yayasan Pondok Pesantren Mamba Unnur Wal Hidayah || <a href="www.linktr.ee/m_amriiin">IT AMRIN</a>
+					</div>
+				</div>
+        	</div><!-- footer -->  
+	    </div><!-- wrapper -->     	
+		<div class="hidden_info"><?php hidden_info(); ?></div>
+		<span class="toggle_info"><i class="icon-wm-maps"></i></span>
+		<?php 
+			wp_footer(); 
+		?>
+	</body>
+</html>
